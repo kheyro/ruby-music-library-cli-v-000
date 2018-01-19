fe415b8820c4b2f7fdb5341ab57bf3b386602e4a
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-19 21:45:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,8 +50,8 @@ class Song
     name = filename.split(/[-.]/).map(&:strip)
     # artist = Artist.find_or_create_by_name(name[0])
     # genre = Genre.find_or_create_by_name(name[2])
-    self.new(name[1])
-    self.artist = name[0]
+    test = self.new(name[1])
+    test.artist = name[0]
   end
 
   def self.create_from_filename(filename)
