d3147c15e12b1be75d1952d2f06f8f8ad2b5bc30
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-19 21:29:59 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new_song = Song.new(name)
+    new_song.save
+    new_song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.find_by_name(song)
+    @@all.detect { |s| s.name == song }
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) ? find_by_name(name) : create(name)
+  end
+
+  def self.new_from_filename(filename)
+    name = filename.split(/[-.]/).map(&:strip)
+    new_song = Song.new(name[1], name[0], name[2])
+    new_song
+  end
+
+  def self.new_from_filename(filename)
+    new_from_filename(filename)
+    save
+  end
+
+end
